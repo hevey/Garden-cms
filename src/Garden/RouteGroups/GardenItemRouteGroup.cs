605f19d321041b6c56e2559156ef8413bf3dc1e7
Glenn Hevey <glenn.hevey@gmail.com>
2023-01-15 18:38:02 +1100

    added get item requests

--- src/Garden/RouteGroups/GardenItemRouteGroup.cs
+++ src/Garden/RouteGroups/GardenItemRouteGroup.cs
@@ -7,10 +7,27 @@ public static class GardenItemRouteGroup
 {
     public static RouteGroupBuilder MapGardenItemRoutes(this RouteGroupBuilder group)
     {
-        group.MapGet("/", async (GardenService gardenService) => await gardenService.GetAsync());
+        group.MapGet("/", GetItems);
+        group.MapGet("/item", GetItem);
 
         return group;
     }
 
+    private static async Task<IResult> GetItem(HttpContext context, GardenService gardenService, string? id, string? name)
+    {
+        if (id is not null)
+            return Results.Ok(await gardenService.GetByIdAsync(id));
+        if (name is not null)
+            return Results.Ok(await gardenService.GetByNameAsync(name));
 
+        return Results.BadRequest("Unable to return item. Provide either a 'id' or 'name' parameter");
+    }
+
+    private static async Task<List<Item>> GetItems(HttpContext context, GardenService gardenService, string? name)
+    {
+        if (name is not null)
+            return await gardenService.GetAllAsync(name);
+            
+        return await gardenService.GetAllAsync();
+    }
 }
