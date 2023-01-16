698827386b4dfd9e94731dab90fb70afb84d0621
Glenn Hevey <glenn.hevey@gmail.com>
2023-01-16 21:22:32 +1100

    added auth Development auth is setup only

--- src/Garden/Program.cs
+++ src/Garden/Program.cs
@@ -7,8 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<MongoStoreDatabaseSettings>("GardenStore", builder.Configuration.GetSection("GardenStoreDatabase"));
 builder.Services.AddScoped<GardenService>();
 
+builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-app.MapGroup("/garden").MapGardenRoutes();
+app.MapGroup("/garden").MapGardenRoutes().RequireAuthorization();
 
 app.Run();
