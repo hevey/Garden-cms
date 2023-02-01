a26ae7df2e4994a87c82b474b9a37f8e440315e9
Glenn Hevey <glenn.hevey@gmail.com>
2023-02-01 21:26:26 +1100

    built out TokenService pages can now check whether token is available added a bunch of css

--- src/Garden.Admin/Program.cs
+++ src/Garden.Admin/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Garden.Admin.Data;
+using Garden.Admin.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<TokenService>();
 
 var app = builder.Build();
 
---
+++ src/Garden.Admin/Services/TokenService.cs
@@ -0,0 +1,50 @@
+using System.Text;
+using System.Text.Json;
+using Garden.Shared.Models;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace Garden.Admin.Services;
+
+public class TokenService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ProtectedLocalStorage _protectedLocalStorage;
+
+    public TokenService(IHttpClientFactory httpClientFactory, ProtectedLocalStorage protectedLocalStorage)
+    {
+        _httpClientFactory = httpClientFactory;
+        _protectedLocalStorage = protectedLocalStorage;
+    }
+
+    public async Task<bool> CheckTokenAsync()
+    {
+        var isAuthenticated = await _protectedLocalStorage.GetAsync<bool>("isAuthenticated");
+
+        if (isAuthenticated is not { Success: true, Value: true }) return false;
+        
+        var tokenResult = await _protectedLocalStorage.GetAsync<string>("token");
+        var token = tokenResult.Success ? tokenResult.Value : null;
+        
+        return token is not null;
+    }
+
+    public async Task<bool> Login(User user)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:7161/garden/identity/login");
+        request.Content = new StringContent(JsonSerializer.Serialize(user), Encoding.UTF8, "application/json");
+
+        var client = _httpClientFactory.CreateClient();
+
+        var response = await client.SendAsync(request);
+        if (!response.IsSuccessStatusCode) return false;
+        
+        await using var responseStream = await response.Content.ReadAsStreamAsync();
+            
+        var token = await JsonSerializer.DeserializeAsync<string>(responseStream);
+        if (token != null) await _protectedLocalStorage.SetAsync("token", token);
+        await _protectedLocalStorage.SetAsync("isAuthenticated", true);
+
+        return true;
+
+    }
+}
