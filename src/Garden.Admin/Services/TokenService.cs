dbfa3893546283698bddd310d9cabdcae73663f5
Glenn Hevey <glenn.hevey@gmail.com>
2023-02-09 20:22:38 +1100

    Setup up a checkauthentication method Check if token is valid server side

--- src/Garden.Admin/Services/TokenService.cs
+++ src/Garden.Admin/Services/TokenService.cs
@@ -2,6 +2,7 @@ using System.Security.Authentication;
 using System.Text;
 using System.Text.Json;
 using Garden.Shared.Models;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 namespace Garden.Admin.Services;
@@ -10,23 +11,51 @@ public class TokenService
 {
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ProtectedLocalStorage _protectedLocalStorage;
+    private readonly NavigationManager _navigationManager;
 
-    public TokenService(IHttpClientFactory httpClientFactory, ProtectedLocalStorage protectedLocalStorage)
+    public TokenService(IHttpClientFactory httpClientFactory, ProtectedLocalStorage protectedLocalStorage, NavigationManager navigationManager)
     {
         _httpClientFactory = httpClientFactory;
         _protectedLocalStorage = protectedLocalStorage;
+        _navigationManager = navigationManager;
+    }
+
+    public async Task CheckAuthentication()
+    {
+        try
+        {
+            await CheckTokenAsync();
+        }
+        catch (HttpRequestException e)
+        {
+            _navigationManager.NavigateTo("/logout");
+        }
+        catch (AuthenticationException e)
+        {
+            _navigationManager.NavigateTo(e.Message == "Not Authenticated" ? "/login" : "/logout");
+        }
     }
 
     public async Task<bool> CheckTokenAsync()
     {
-        var isAuthenticated = await _protectedLocalStorage.GetAsync<bool>("isAuthenticated");
+        var token = await GetToken();
 
-        if (isAuthenticated is not { Success: true, Value: true }) return false;
+        TokenDTO tokenDto = new TokenDTO(token);
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:7161/garden/identity/validate");
+        request.Content = new StringContent(JsonSerializer.Serialize(tokenDto), Encoding.UTF8, "application/json");
+
+        var client = _httpClientFactory.CreateClient();
+
+        var response = await client.SendAsync(request);
+        await using var responseStream = await response.Content.ReadAsStreamAsync();
+
+        if (response.IsSuccessStatusCode) return true;
         
-        var tokenResult = await _protectedLocalStorage.GetAsync<string>("token");
-        var token = tokenResult.Success ? tokenResult.Value : null;
-        
-        return token is not null;
+        await _protectedLocalStorage.DeleteAsync("isAuthenticated");
+        await _protectedLocalStorage.DeleteAsync("token");
+        throw new HttpRequestException();
+
     }
 
     public async Task<string> GetToken()
